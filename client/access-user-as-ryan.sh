e3a929b1afd3a108766ab724cc08e08f35b3c217
Alfred Matthews <asm13243546@gmail.com>
2017-05-25 05:24:06 +0000

    restores ryan

--- client/access-user-as-ryan.sh
+++ client/access-user-as-ryan.sh
@@ -18,5 +18,5 @@
 
 rm -rf work
 mkdir work
-wget --http-user=bob --http-password=bobspassword --directory-prefix=work  --output-file=work/log.txt http://localhost:8080/camel/admin
+wget --http-user=ryan --http-password=ryanspassword --directory-prefix=work --output-file=work/log.txt http://localhost:8080/camel/user
 cat -n work/* | less
--- client/access-user-as-ryancameron.sh
+++ client/access-user-as-ryancameron.sh
@@ -18,5 +18,5 @@
 
 rm -rf work
 mkdir work
-wget --http-user=jim --http-password=jimspassword --directory-prefix=work  --output-file=work/log.txt http://localhost:8080/camel/admin
+wget --http-user=ryancameron --http-password=ryancameronspassword --directory-prefix=work --output-file=work/log.txt http://localhost:8080/camel/user
 cat -n work/* | less
